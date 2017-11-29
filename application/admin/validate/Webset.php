cb625f3b1d1d34dd5c107df51ab41b88d36f0506
hline123 <2122948766@qq.com>
2017-11-29 09:58:48 +0800

    新增配置验证信息

--- application/admin/validate/Webset.php
+++ application/admin/validate/Webset.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\admin\validate;
 
 use think\Validate;
@@ -10,12 +11,24 @@ use think\Validate;
  */
 class Webset extends Validate
 {
-    protected $rule = [
-        'cname' => 'require',
-        'ename' => 'require',
-    ];
-    protected $message = [
-        'cname.require' => '请输入配置中文名称',
-        'ename.require' => '请输入配置英文名称',
-    ];
+    protected $rule
+        = [
+            'cname'   => 'require|unique:webset|max:60',
+            'ename'   => 'require|unique:webset|max:60',
+            'dt_type' => 'require|number',
+            'cf_type' => 'require|number',
+        ];
+    protected $message
+        = [
+            'cname.require'   => '请输入配置中文名称',
+            'cname.unique'    => '配置中文名称不能重复',
+            'cname.max'       => '配置中文名称长度不得超过60个字符',
+            'ename.require'   => '请输入配置英文名称',
+            'ename.unique'    => '配置英文名称不能为空',
+            'ename.max'       => '配置英文名称长度不得超过60个字符',
+            'dt_type.require' => '配置选择类型不能为空',
+            'dt_type.number'  => '配置选择类型必须为数字类型',
+            'cf_type.require' => '配置分类不能为空',
+            'cf_type.number'  => '配置分类必须为数字类型',
+        ];
 }
