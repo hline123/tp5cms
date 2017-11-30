58b0c6c17fff7fd790e3d92071f7fc9cd4bdd240
hline123 <2122948766@qq.com>
2017-11-30 16:04:22 +0800

    栏目模板处理

--- application/admin/model/Category.php
+++ application/admin/model/Category.php
@@ -6,5 +6,9 @@ use think\Model;
 
 class Category extends Model
 {
-    //
+    //定义主键
+    protected $pk = 'cate_id';
+    // 定义数据表
+    protected $table = 'tp_cate';
+
 }
--- public/static/admin/js/common.js
+++ public/static/admin/js/common.js
@@ -35,3 +35,9 @@ function del(obj, id, url) {
         }
     });
 }
+$(function () {
+    $('input[type="checkbox"], input[type="radio"]').iCheck({
+        checkboxClass: 'icheckbox_flat-green',
+        radioClass: 'iradio_flat-green'
+    })
+});
