086a6fc90f88fbedf6bd8147663b674b59f786ca
hline123 <2122948766@qq.com>
2017-12-05 11:38:32 +0800

    完成栏目首页ajax排序，修改状态操作

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -3,7 +3,7 @@
 namespace app\admin\controller;
 
 use app\admin\model\Category as CategoryModel;
-use houdunwang\arr\Arr;
+
 
 /**
  * 栏目管理控制器类
@@ -16,14 +16,20 @@ class Category extends Base
      * 栏目首页处理
      * @return \think\response\View
      */
-    public function index()
+    public function index(CategoryModel $category)
     {
         //输出栏目列表
-        $lists = [];
+        $lists = $category->getAll();
+        //halt($lists);
         $this->assign('lists', $lists);
         return view('index');
     }
 
+    /**
+     * 添加栏目
+     * @param CategoryModel $category
+     * @return \think\response\View
+     */
     public function store (CategoryModel $category)
     {
         // 接收栏目添加信息
@@ -37,14 +43,15 @@ class Category extends Base
             }
         }
         // 获取栏目数据
-        $cateRes = db('cate')->select();
-        // halt($cateRes);
-        // 变更成树状结构
-        $_cateRes = Arr::tree($cateRes, 'cate_name', 'cate_id', 'cate_pid');
-        //halt($_cateRes);
+        $_cateRes = $category->getAll();
         $this->assign('cateRes', $_cateRes);
         return view('store');
     }
+
+    /**
+     * ajax 处理文件上传
+     * @return string
+     */
     public function upImg ()
     {
         // 获取上传文件
@@ -66,6 +73,39 @@ class Category extends Base
             return json_encode(['valid'=>1, 'msg'=> $path]);
         }else {
             return json_encode(['valid'=>0, 'msg'=> $file->getError()]);
+        }
+    }
+
+    /**
+     * ajax 改变栏目状态
+     * @return int
+     */
+    public function changeStatus()
+    {
+        $id = input('param.id');
+        $cateAttr = db('cate')->where('cate_id', $id)->value('cate_status');
+        if ($cateAttr == 1) {
+            db('cate')->where('cate_id', $id)->update(['cate_status'=>0]);
+            return 1;
+        }else {
+            db('cate')->where('cate_id', $id)->update(['cate_status'=>1]);
+            return 0;
+        }
+    }
+
+    /**
+     * ajax 排序
+     * @return int
+     */
+    public function changeSort ()
+    {
+        if (request()->isAjax()) {
+            $data = input('param.');
+            $result = db('cate')->where('cate_id', $data['id'])->update(['cate_sort'=>$data['cate_sort']]);
+            if ($result !== false) {
+                // 倒序排列
+                return 1;
+            }
         }
     }
     public function delete()
--- application/admin/model/Category.php
+++ application/admin/model/Category.php
@@ -2,6 +2,7 @@
 
 namespace app\admin\model;
 
+use houdunwang\arr\Arr;
 use think\Model;
 
 /**
@@ -16,6 +17,24 @@ class Category extends Model
     // 定义数据表
     protected $table = 'tp_cate';
 
+    /**
+     * 属性获取栏目所有数据
+     * @return mixed
+     */
+    public function getAll ()
+    {
+        // 获取栏目数据
+        $data = $this->order('cate_sort desc,cate_id')->select();
+        // 变更树形结构
+        $result = Arr::tree($data, 'cate_name', 'cate_id', 'cate_pid');
+        return $result;
+    }
+
+    /**
+     * 添加栏目
+     * @param $data
+     * @return array
+     */
     public function store($data)
     {
         // 验证数据
